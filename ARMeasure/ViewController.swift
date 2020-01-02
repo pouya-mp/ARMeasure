e93eaddee9745d0b85619b8ae496ddc50b445f48
Pouya <mazloumpour.pouya@gmail.com>
2020-01-02 14:31:32 +0330

    complete project app

--- ARMeasure/ViewController.swift
+++ ARMeasure/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
     var dotnNodes = [SCNNode]()
+    var textNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +31,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touchLocation = touches.first?.location(in: sceneView) {
+            
+            if dotnNodes.count >= 2{
+                for dot in dotnNodes{
+                    dot.removeFromParentNode()
+                }
+                dotnNodes = [SCNNode]()
+            }
             
             let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
             if let hitResult = hitTestResults.first {
@@ -77,9 +85,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func updateText(text : String, atPosition position :SCNVector3){
         
+        textNode.removeFromParentNode()
+        
         let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
         textGeometry.firstMaterial?.diffuse.contents = UIColor.cyan
-        let textNode = SCNNode(geometry: textGeometry)
+        textNode = SCNNode(geometry: textGeometry)
         textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
         textNode.scale = SCNVector3(0.01, 0.01, 0.01)
         sceneView.scene.rootNode.addChildNode(textNode)
